8c0c86f49c73056cbfa5aa7587996c7475053293
randomstuff69 <132950785+randomstuff69@users.noreply.github.com>
2023-05-11 08:50:48 -0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+    "name": "Pr0xyUniverse.github.io",
+    "version": "1.0.0",
+    "description": "",
+    "scripts": {
+        "build": "echo \"No build specified\" && exit 1",
+        "test": "echo \"No test specified\" && exit 1",
+        "start": "node index.js"
+    },
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/randomstuff69/Pr0xyUniverse.github.io.git"
+    },
+    "author": "",
+    "license": "ISC",
+    "bugs": {
+        "url": "https://github.com/randomstuff69/Pr0xyUniverse.github.io/issues"
+    },
+    "homepage": "https://github.com/randomstuff69/Pr0xyUniverse.github.io/"
+}
